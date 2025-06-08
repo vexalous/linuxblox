a301295746acc2fd340b5fa66cd972f0e07c5f0b
vexalous <vexalous@hotmail.com>
2025-06-08 18:54:12 -0400

    Update app.axaml.cs

--- app.axaml.cs
+++ app.axaml.cs
@@ -1,7 +1,7 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
-using Avalonia.ReactiveUI; // <-- This is the missing line that fixes the error.
+using Avalonia.ReactiveUI;
 using linuxblox.viewmodels;
 using ReactiveUI;
 using Splat;
@@ -17,7 +17,6 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
-        // This line registers the activation helper for Avalonia.
         Locator.CurrentMutable.Register(() => new AvaloniaActivationForViewFetcher(), typeof(IActivationForViewFetcher));
         
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
