98e31d27925c89ff008dd7a6db78efd2cc8e7b87
vexalous <vexalous@hotmail.com>
2025-06-08 17:58:45 -0400

    Update app.axaml.cs

--- app.axaml.cs
+++ app.axaml.cs
@@ -1,6 +1,9 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using linuxblox.viewmodels;
+using ReactiveUI;
+using Splat;
 
 namespace linuxblox;
 
@@ -13,9 +16,16 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
+        // This line registers the activation helper for Avalonia.
+        Locator.CurrentMutable.Register(() => new AvaloniaActivationForViewFetcher(), typeof(IActivationForViewFetcher));
+        
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
             desktop.MainWindow = new MainWindow();
+        }
+        else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
+        {
+            singleViewPlatform.MainView = new MainWindow();
         }
 
         base.OnFrameworkInitializationCompleted();
